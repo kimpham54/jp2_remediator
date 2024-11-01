47f549666d2dc331c05e3b870cff89b1e0fbbdea
Andrew Woods <awoods01@gmail.com>
2024-11-01 14:44:24 +0100

    Replace print statements with logging (#4)
    * Replace print statements with logging
    
    - defaults to only console logging
    - defaults to WARNING level
       - configurable with: `export APP_LOG_LEVEL=[INFO|DEBUG]`
    
    * Merge awoods-logging into logging (#3)
    
    * linter for box reader after logging
    
    * comment out diff cover
    
    remove socket
    
    * skip processing of files where n value is not 1
    
    * tests with 65% cvg
    
    * add in github actions install of diff cover globally
    
    * update paths for diff cover
    
    * test diff cover install and check path
    
    * uncomment diff cover test
    
    * uncomment diff cover test 2
    
    * new coverage test
    
    * full test coverage
    
    * flake8 passes all files
    
    * check flake8 version
    
    * mod flake8 command for 120 line length
    
    * add additional test coverage, stops early if not 85
    
    ---------
    
    Co-authored-by: kim pham <kimpham54@users.noreply.github.com>

--- src/jp2_remediator/main.py
+++ src/jp2_remediator/main.py
@@ -1,34 +1,32 @@
-import sys
-import os
+import argparse
+from jp2_remediator.box_reader import BoxReader, process_directory, process_s3_bucket
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <folder_path1> <folder_path2>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="JP2 file processor")
+    parser.add_argument("--file", help="Path to a single JP2 file to process.")
+    parser.add_argument(
+        "--directory", help="Path to a directory of JP2 files to process."
+    )
+    parser.add_argument(
+        "--bucket", help="Name of the AWS S3 bucket to process JP2 files from."
+    )
+    parser.add_argument(
+        "--prefix", help="Prefix of files in the AWS S3 bucket (optional)."
+    )
 
-    folder_path1 = sys.argv[1]
-    folder_path2 = sys.argv[2]
+    args = parser.parse_args()
 
-    if not os.path.isdir(folder_path1):
-        print(f"Error: {folder_path1} is not a valid directory.")
-        sys.exit(1)
-
-    if not os.path.isdir(folder_path2):
-        print(f"Error: {folder_path2} is not a valid directory.")
-        sys.exit(1)
-
-    print(f"Folder 1: {folder_path1}")
-    print(f"Folder 2: {folder_path2}")
+    if args.file:
+        reader = BoxReader(args.file)
+        reader.read_jp2_file()
+    elif args.directory:
+        process_directory(args.directory)
+    elif args.bucket:
+        process_s3_bucket(args.bucket, args.prefix)
+    else:
+        print("Please specify either --file, --directory, or --bucket.")
 
 
 if __name__ == "__main__":
     main()
-
-
-def hello_world():
-    print("Hello, world!")
-
-
-def add_one(number):
-    return number + 1
